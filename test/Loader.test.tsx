6a3753192955e19837475468ca1cbc22db7ac439
wwesleyalves <97242196+wwesleyalves@users.noreply.github.com>
2023-08-30 14:22:09 -0300

    Update Loader.test.tsx

--- test/Loader.test.tsx
+++ test/Loader.test.tsx
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Página Vulnerável</title>
-</head>
-<body>
-    <h1>Bem-vindo</h1>
-    <p>Olá, <span id="nome"></span>!</p>
-    <script>
-        var nome = prompt("Digite seu nome:");
-        document.getElementById("nome").textContent = nome;
-    </script>
-</body>
-</html>
+const userInput = "<script>alert('XSS Attack');</script>";
+const message = `Hello, ${userInput}!`;
+
+document.getElementById("output").innerHTML = message;
