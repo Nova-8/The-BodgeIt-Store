c2660aaeb50f52d9d08cb91b0caa73c9b4b9efea
wwesleyalves <97242196+wwesleyalves@users.noreply.github.com>
2023-09-15 15:41:51 -0300

    Update and rename vul_.java to vul.js

--- vul_.java
+++
@@ -1,4 +0,0 @@
-String username = "steve";
-String password = "blue";
-Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-                  "user=" + username + "&password=" + password); // Sensitive
